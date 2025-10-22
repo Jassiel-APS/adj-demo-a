091c207499104e4ab7cdd2b0df77bd789ba92a90
Jassiel-APS <20223tn073@utez.edu.mx>
2025-10-21 23:46:20 -0600

    [ADD] url 3

--- client/src/modules/test/test.controller.js
+++ client/src/modules/test/test.controller.js
@@ -1,10 +1,10 @@
 const TestController = {};
 const ENV = import.meta.env;
 
-const API_URL = 'http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}';
+const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
 
 TestController.callToAPI = async () => {
-    await fetch('${API_URL}/test',{
+    await fetch(`${API_URL}/test`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
